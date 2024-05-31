558909464318e117bb4b199e3f1a02e1836c31f8
Dddddd <1972218362@qq.com>
2024-05-31 22:43:02 +0800

    静态限速修正

--- main.py
+++ main.py
@@ -38,32 +38,48 @@ for index, row in df_curve.iterrows():
 sorted_data = sorted(zip(x_values, y_values))
 x_values_sorted, y_values_sorted = zip(*sorted_data)
 
+
 # 插入缺失的 y 值，默认设置为 87
 final_x_values = []
 final_y_values = []
-previous_y = 87
+previous_y = None
+# previous_y = 87
 
 for x, y in sorted_data:
-    if final_x_values and x != final_x_values[-1]:
+    if x == 18975.905:
+        final_x_values.append(previous_x)
+        final_y_values.append(87)  # 设置缺失的 y 值为 87
         final_x_values.append(x)
-        final_y_values.append(previous_y)
+        final_y_values.append(87)  # 设置缺失的 y 值为 87
+
+    else:
+        if previous_y is not None and y != previous_y:
+            if x > previous_x:
+                # 插入从 previous_x 到 x 之间的缺失值
+                final_x_values.append(previous_x)
+                final_y_values.append(87)  # 设置缺失的 y 值为 87
+                final_x_values.append(x)
+                final_y_values.append(87)  # 设置缺失的 y 值为 87
     final_x_values.append(x)
     final_y_values.append(y)
-    previous_y = y
 
+    previous_x = x
+    previous_y = y
 # 绘制限速区间图
-fig, ax = plt.subplots(figsize=(22, 10))
+fig, ax = plt.subplots(figsize=(82, 15))
 
 # 绘制连续的折线
-ax.plot(final_x_values, final_y_values, color='darkblue')
+for i in range(1, len(final_x_values)):
+    if final_x_values[i] == final_x_values[i - 1] or final_y_values[i] == final_y_values[i - 1]:
+        ax.plot([final_x_values[i - 1], final_x_values[i]], [final_y_values[i - 1], final_y_values[i]], color='darkblue')
 
-# # 为每个限速区间绘制一条水平线
-# line_color = 'blue'  # 设置线条颜色
-# for index, row in df_station.iterrows():
-#     line_color = 'blue'  # 设置线条颜色
-#     ax.plot([row['限速起点（m）'], row['限速终点（m）']], [row['限速值（km/h）'], row['限速值（km/h）']], label=row['站台名'],color=line_color)
-# for index, row in df_curve.iterrows():
-#      ax.plot([row['限速起点（m）'], row['限速终点（m）']], [row['限速值（km/h）'], row['限速值（km/h）']], color='red')
+# 为每个限速区间绘制一条水平线
+# 这个是用红色表示它有的数据，暂时这样方便检查有误的地方
+for index, row in df_station.iterrows():
+    line_color = 'blue'  # 设置线条颜色
+    ax.plot([row['限速起点（m）'], row['限速终点（m）']], [row['限速值（km/h）'], row['限速值（km/h）']], label=row['站台名'],color='red')
+for index, row in df_curve.iterrows():
+     ax.plot([row['限速起点（m）'], row['限速终点（m）']], [row['限速值（km/h）'], row['限速值（km/h）']], color='red')
 
 # 添加正常的横坐标刻度
 ax.set_xticks(range(0, int(df_station['限速终点（m）'].max()) + 1000, 1000))
