6fe27e21b31bcd951febd8eb4b9d6dd5b5785a55
Dddddd <1972218362@qq.com>
2024-06-05 21:32:14 +0800

    sbi三组曲线单独绘制

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 from matplotlib.font_manager import FontProperties
-
+import numpy as np
 # 设置中文字体路径
 font_path = '/System/Library/Fonts/STHeiti Medium.ttc'
 font_prop = FontProperties(fname=font_path)
@@ -13,7 +13,7 @@ plt.rcParams['font.sans-serif'] = [font_prop.get_name()]
 plt.rcParams['axes.unicode_minus'] = False  # 解决负号显示问题
 
 # 读取 Excel 文件中的特定工作表
-excel_file = '/Users/dingyuhan/Documents/课程学习/大三下/railway/线路条件数据.xlsx'  # 确保文件路径正确
+excel_file = '线路条件数据.xlsx'  # 确保文件路径正确
 df_station = pd.read_excel(excel_file, sheet_name='station')
 df_curve = pd.read_excel(excel_file, sheet_name='curve')
 # 打印表头
@@ -66,12 +66,36 @@ for x, y in sorted_data:
     previous_x = x
     previous_y = y
 # 绘制限速区间图
-fig, ax = plt.subplots(figsize=(82, 15))
+fig, ax = plt.subplots(figsize=(132, 15))
 
 # 绘制连续的折线
 for i in range(1, len(final_x_values)):
     if final_x_values[i] == final_x_values[i - 1] or final_y_values[i] == final_y_values[i - 1]:
         ax.plot([final_x_values[i - 1], final_x_values[i]], [final_y_values[i - 1], final_y_values[i]], color='darkblue')
+
+
+a=0.5
+for i in range(1, len(final_x_values)):
+    if final_x_values[i] == final_x_values[i - 1] or final_y_values[i] == final_y_values[i - 1]:
+        ax.plot([final_x_values[i - 1], final_x_values[i]], [final_y_values[i - 1]-8, final_y_values[i]-8], color='green')
+    if final_y_values[i] < final_y_values[i - 1]:
+        x_end = final_x_values[i]
+        x_start = x_end - 400
+        x_curve = np.linspace(x_start, x_end, 100)
+        y_curve = np.sqrt(2 * a * (x_end - x_curve) + (final_y_values[i]-8) * (final_y_values[i]-8) / (3.6 * 3.6)) * 3.6
+        ax.plot(x_curve, y_curve, color='green')
+# 在每个车站终点前800米范围内绘制根号下2ax的曲线，a=1.2
+a = 0.5
+for index, row in df_station.iterrows():
+    x_end = row['限速终点（m）']
+    x_start = x_end - 500
+    x_curve = np.linspace(x_start, x_end, 100)
+    y_curve = np.sqrt(2 * a * (x_end - x_curve))*3.6
+    ax.plot(x_curve, y_curve, color='green')
+
+
+
+
 
 # 为每个限速区间绘制一条水平线
 # 这个是用红色表示它有的数据，暂时这样方便检查有误的地方
@@ -81,14 +105,24 @@ for index, row in df_station.iterrows():
 for index, row in df_curve.iterrows():
      ax.plot([row['限速起点（m）'], row['限速终点（m）']], [row['限速值（km/h）'], row['限速值（km/h）']], color='red')
 
+
+
+
+
+
+
+
 # 添加正常的横坐标刻度
 ax.set_xticks(range(0, int(df_station['限速终点（m）'].max()) + 1000, 1000))
 
 # 在站台位置添加矩形和站台名称
 for index, row in df_station.iterrows():
-    midpoint = (row['限速起点（m）'] + row['限速终点（m）']) / 2
-    rect = Rectangle((midpoint - 50, -10), 100, 10, linewidth=1, edgecolor='black', facecolor='yellow', alpha=0.5)
+    rect_start = row['限速起点（m）']
+    rect_end = row['限速终点（m）']
+    rect_width = rect_end - rect_start
+    rect = Rectangle((rect_start, -10), rect_width, 10, linewidth=1, edgecolor='black', facecolor='yellow', alpha=0.5)
     ax.add_patch(rect)
+    midpoint = (rect_start + rect_end) / 2
     ax.text(midpoint, -15, row['站台名'], ha='center', fontproperties=font_prop)
 
 plt.xlabel('距离（m）', fontproperties=font_prop)
